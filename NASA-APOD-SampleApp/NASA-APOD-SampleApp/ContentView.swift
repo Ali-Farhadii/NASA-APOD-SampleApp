a9c8f52ec6e54df51d93f367acd70a0ccb081386
Ali Farhadi <ali.farhadi@snapp.cab>
2024-11-15 10:37:59 +0000

    Init Xcode project

---
+++ NASA-APOD-SampleApp/NASA-APOD-SampleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NASA-APOD-SampleApp
+//
+//  Created by Ali Farhadi on 11/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NASA-APOD-SampleApp/NASA-APOD-SampleApp/NASA_APOD_SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASA_APOD_SampleAppApp.swift
+//  NASA-APOD-SampleApp
+//
+//  Created by Ali Farhadi on 11/15/24.
+//
+
+import SwiftUI
+
+@main
+struct NASA_APOD_SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
