af2c5fa4cd57762560ff602df3f3687b7acdff93
Ali Farhadi <ali.farhadi@snapp.cab>
2024-11-19 21:27:53 +0000

    Fetch APOD via URLSession dataSource

--- NASA-APOD-SampleApp/NASA-APOD-SampleApp/AppModules/APOD/DataLayer/DataSources/Remote/APODRemoteDataSource.swift
+++ NASA-APOD-SampleApp/NASA-APOD-SampleApp/AppModules/APOD/DataLayer/DataSources/Remote/APODRemoteDataSource.swift
@@ -13,13 +13,24 @@ protocol APODRemoteDataSource {
 
 struct APODURLSessionDataSource: APODRemoteDataSource {
     
-    let networkService: NetworkService
+    private let networkService: NetworkService
     
     init(networkService: NetworkService) {
         self.networkService = networkService
     }
     
     func fetchAPOD(with date: Date) async throws -> APODBusinessModel {
-        APODBusinessModel()
+        let apodEndpoint = APODEndpoint()
+        let response: APODDecodableModel = try await networkService.request(with: apodEndpoint)
+        return mapToAPODBusinessModel(response)
+    }
+    
+    func mapToAPODBusinessModel(_ response: APODDecodableModel) -> APODBusinessModel {
+        APODBusinessModel(title: response.title,
+                          copyright: response.copyright,
+                          date: response.date,
+                          explanation: response.explanation,
+                          url: response.url,
+                          mediaType: response.mediaType)
     }
 }
