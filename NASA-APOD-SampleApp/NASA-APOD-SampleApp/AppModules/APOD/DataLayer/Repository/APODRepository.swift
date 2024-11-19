003e4d8ca211a1d1f7872789f3959f460f75c3b0
Ali Farhadi <ali.farhadi@snapp.cab>
2024-11-19 12:41:51 +0000

    Implement basic Clean MVVM architecture

---
+++ NASA-APOD-SampleApp/NASA-APOD-SampleApp/AppModules/APOD/DataLayer/Repository/APODRepository.swift
@@ -0,0 +1,8 @@
+//
+//  APODRepository.swift
+//  NASA-APOD-SampleApp
+//
+//  Created by Ali Farhadi on 11/19/24.
+//
+
+import Foundation
--- NASA-APOD-SampleApp/NASA-APOD-SampleApp/AppModules/APOD/PresentationLayer/View/APODView.swift
+++ NASA-APOD-SampleApp/NASA-APOD-SampleApp/AppModules/APOD/PresentationLayer/View/APODView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  APODView.swift
 //  NASA-APOD-SampleApp
 //
 //  Created by Ali Farhadi on 11/15/24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct APODView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    APODView()
 }
