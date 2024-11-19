6d6ae33683ef17559ab5a9e96a4a8ed0ee054a5b
Ali Farhadi <ali.farhadi@snapp.cab>
2024-11-19 13:11:23 +0000

    Add Constants struct for global constans
    - This file should be modify later to use Xcode Configs

---
+++ NASA-APOD-SampleApp/NASA-APOD-SampleApp/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  NASA-APOD-SampleApp
+//
+//  Created by Ali Farhadi on 11/19/24.
+//
+
+import Foundation
+
+struct Constants {
+    static let baseURL = ""
+    static let apiKey = ""
+}
