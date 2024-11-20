714ba684b735a487e8b91b1810e20d584d4d4423
Ali Farhadi <ali.farhadi@snapp.cab>
2024-11-20 14:53:29 +0000

    Get constants properties from Xcode Config files

--- NASA-APOD-SampleApp/NASA-APOD-SampleApp/Utilities/Constants.swift
+++ NASA-APOD-SampleApp/NASA-APOD-SampleApp/Utilities/Constants.swift
@@ -8,6 +8,17 @@
 import Foundation
 
 struct Constants {
-    static let baseURL = "https://api.nasa.gov"
-    static let apiKey = "egq8Cs4NeS3HKBl99YzEmYbO3kz02ZNhmHMZQV1e" // "DEMO_KEY"
+    static let apiKey: String = {
+        guard let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String else {
+            fatalError("API_KEY not found in Info.plist")
+        }
+        return apiKey
+    }()
+    
+    static let baseURL: String = {
+        guard let baseURL = Bundle.main.infoDictionary?["BASE_URL"] as? String else {
+            fatalError("BASE_URL not found in Info.plist")
+        }
+        return baseURL
+    }()
 }
